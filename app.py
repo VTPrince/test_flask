8a7abc13c5b925023d7087135a27fba1338d82ed
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-02 22:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import request,Flask
 from flask import jsonify
+import geocoder
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def main():
     if 'X-Forwarded-For' in request.headers:
         proxy_data = request.headers['X-Forwarded-For']
         ip_list = proxy_data.split(',')
-        user_ip = 'np'+ip_list[0]  # first address in list is User IP
+        user_ip = ip_list[0]  # first address in list is User IP
     else:
-        user_ip = 'tp'+request.remote_addr
-    return '<h1>ip : '+ user_ip #request.remote_addr
+        user_ip = request.remote_addr
+    ip = geocoder.ip(user_ip)
+    return '<h1>ip : '+ str(ip.city) #request.remote_addr
 if __name__ == '__main__':
     app.run(debug=True)
