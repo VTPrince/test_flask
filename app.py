423e37b069e8b87f91835376632d44dade649eba
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-02 21:36:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 
 def main():
-    return '<h1>ip : '+request.remote_addr
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        return '<h1>new ip : '+request.environ['REMOTE_ADDR'])
+    else:
+        return '<h1>othernew ip : '+request.environ['HTTP_X_FORWARDED_FOR'])
 if __name__ == '__main__':
     app.run(debug=True)
