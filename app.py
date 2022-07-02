98ee3d5e1c9186f54fed9e30a1cdaad644966d43
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-02 21:56:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     if 'X-Forwarded-For' in request.headers:
         proxy_data = request.headers['X-Forwarded-For']
         ip_list = proxy_data.split(',')
-        user_ip = 'np'+ip_list[1]  # first address in list is User IP
+        user_ip = 'np'+ip_list[0]  # first address in list is User IP
     else:
         user_ip = 'tp'+request.remote_addr
     return '<h1>ip : '+ user_ip #request.remote_addr
